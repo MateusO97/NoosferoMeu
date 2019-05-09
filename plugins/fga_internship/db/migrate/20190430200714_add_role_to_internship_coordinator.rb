61fe952472fc0378e08d044cc7b801604cb0ac8e
Vinícius Cantuária <cantuariavc@gmail.com>
2019-05-09 16:50:44 -0300

    Add role to internship coordinator
    Co-authored-by: mendesiasmin <mendesiasmin96@gmail.com>

---
+++ plugins/fga_internship/db/migrate/20190430200714_add_role_to_internship_coordinator.rb
@@ -0,0 +1,5 @@
+class AddRoleToInternshipCoordinator < ActiveRecord::Migration[5.1]
+  def change
+    Role.create!(name: 'Internship Coordinator', key: 'internship_coordinator', permissions: ['manage_internship'], environment: Environment.default())
+  end
+end
