a1738e647e4b8b35059dfe39c9f2ed475b9a7c5f
Iasmin Mendes <mendesiasmin96@gmail.com>
2019-05-13 20:31:20 -0300

    Apply stylesheet in migration
    Co-authored-by: Vinícius Cantuária <cantuariavc@gmail.com>

--- plugins/fga_internship/db/migrate/20190430200714_add_role_to_internship_coordinator.rb
+++ plugins/fga_internship/db/migrate/20190430200714_add_role_to_internship_coordinator.rb
@@ -1,5 +1,10 @@
 class AddRoleToInternshipCoordinator < ActiveRecord::Migration[5.1]
   def change
-    Role.create!(name: 'Internship Coordinator', key: 'internship_coordinator', permissions: ['manage_internship'], environment: Environment.default())
+    Role.create!(
+      name: 'Internship Coordinator',
+      key: 'internship_coordinator',
+      permissions: ['manage_internship'],
+      environment: Environment.default
+    )
   end
 end
