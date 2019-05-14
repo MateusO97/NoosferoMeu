be6498afb61b4d25d653a32882027fbd4fb8edb1
Vinícius Cantuária <cantuariavc@gmail.com>
2019-05-14 15:18:37 -0300

    Fix iternship coordinator migration
    Co-authored-by: mendesiasmin <mendesiasmin96@gmail.com>

--- plugins/fga_internship/db/migrate/20190430200714_add_role_to_internship_coordinator.rb
+++ plugins/fga_internship/db/migrate/20190430200714_add_role_to_internship_coordinator.rb
@@ -1,18 +1,16 @@
 class AddRoleToInternshipCoordinator < ActiveRecord::Migration[5.1]
   def change
     reversible do |dir|
-      change_table :products do |t|
-        dir.up do
-          Role.create!(
-            name: 'Internship Coordinator',
-            key: 'profile_internship_coordinator',
-            permissions: ['manage_internship'],
-            environment: Environment.default
-          )
-        end
-        dir.down do
-          Role.find_by(name: 'Internship Coordinator').destroy!
-        end
+      dir.up do
+        Role.create!(
+          name: 'Internship Coordinator',
+          key: 'profile_internship_coordinator',
+          permissions: ['manage_internship'],
+          environment: Environment.default
+        )
+      end
+      dir.down do
+        Role.find_by(name: 'Internship Coordinator').destroy!
       end
     end
   end
