3d3a802ee5206cffd1fa6d630af6b2c84fb3435f
Iasmin Mendes <mendesiasmin96@gmail.com>
2019-05-13 21:07:22 -0300

    Add reversible method to migration

--- plugins/fga_internship/db/migrate/20190430200714_add_role_to_internship_coordinator.rb
+++ plugins/fga_internship/db/migrate/20190430200714_add_role_to_internship_coordinator.rb
@@ -1,10 +1,19 @@
 class AddRoleToInternshipCoordinator < ActiveRecord::Migration[5.1]
   def change
-    Role.create!(
-      name: 'Internship Coordinator',
-      key: 'internship_coordinator',
-      permissions: ['manage_internship'],
-      environment: Environment.default
-    )
+    reversible do |dir|
+      change_table :products do |t|
+        dir.up do
+          Role.create!(
+            name: 'Internship Coordinator',
+            key: 'profile_internship_coordinator',
+            permissions: ['manage_internship'],
+            environment: Environment.default
+          )
+        end
+        dir.down do
+          Role.find_by(name: 'Internship Coordinator').destroy!
+        end
+      end
+    end
   end
 end
